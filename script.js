c39b3a7067e96ae7d7dec53d04ac3f15a8588762
abdellahak <93621937+abdellahak@users.noreply.github.com>
2024-07-04 00:35:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 let header = document.querySelector('header');
+let secondNav = document.querySelector('.nav');
 
 window.addEventListener('scroll',function(e){
     if(window.scrollY>300){
         header.classList.add('colored-nav');
+        secondNav.classList.add('colored-nav');
+        secondNav.style.height = 'fit-content';
     }else{
         header.classList.remove('colored-nav');
+        secondNav.classList.remove('colored-nav');
+        secondNav.style.height = 'fit-content';
     }
 })
 
 function showMenu(){
-    
+    secondNav.classList.toggle('hide');
+}
+
+window.onresize = function(){
+    secondNav.classList.add('hide');
 }
